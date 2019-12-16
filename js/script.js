6a026bc1fedca4f455c04db523c6ba851472216b
garethryan <garethalexander@me.com>
2019-12-16 10:59:09 -0800

    function and print quote revisions
    added additional objects to the array to make 5 quotes.

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ project 1 - A Random Quote Generator
  ***/
 var quote = ''; 
 
+
 var quotes = [{Quote: ' When something is important enough, you do it even if the odds are not in your favor',
               Source: 'Elon Musk', 
               Citation: 'www.cbsnews.com ', 
@@ -33,9 +34,24 @@ var quotes = [{Quote: ' When something is important enough, you do it even if th
                Year: ' Unknown ', 
                Category: 'Comedy',
             },  
+            {Quote: 'Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.', 
+            Source:  'Michael Scott (Steve Carell),',
+            Citation: ' The Office, Season 2: The Fight.', 
+            Year: 'Unknown',  
+            Category: 'TV',
+              },
+
+            {
+              Quote: 'If you obsess over whether you are making the right decision, you are basically assuming that the universe will reward you for one thing and punish you for another.', 
+              Source: 'Deepak Chopra',
+              Citation: 'The Book of Secrets: Unlocking the Hidden Dimensions of Your Life',
+              Year: '2005',
+              Category: 'Spiritual',
+            }, 
+
 ]
  
-  var i = 1; i < quotes.length ; i ++; 
+  var i = 0; i < quotes.length ; i ++; 
     
    {
     var quote = quotes[i]+= quotes.Source + quotes.Citation + quotes.Year + quotes.Category;
@@ -52,18 +68,20 @@ function getRandomQuote(){
    var randomQuote = quotes[getRandomNumber]; 
 
    return randomQuote;  
-   
+    
 }
-
-
+console.log(getRandomQuote());
+ 
 /***
  * `printQuote` function
 ***/ 
 function printQuote(){ 
    var randomQuote = getRandomQuote(); 
   return(randomQuote); 
+  
 } 
-console.log(printQuote); 
+console.log(printQuote()); 
+ 
    
 
 /***
