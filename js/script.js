6e8427fd00763fa32c79adf5f6099f7f4f60590a
garethryan <garethalexander@me.com>
2019-12-17 10:06:47 -0800

    updated dot notation on

--- js/script.js
+++ js/script.js
@@ -14,50 +14,46 @@ project 1 - A Random Quote Generator
 var quote = ''; 
 
 
-var quotes = [{Quote: ' When something is important enough, you do it even if the odds are not in your favor',
-              Source: 'Elon Musk', 
-              Citation: 'www.cbsnews.com ', 
-              Year: 'May 2012', 
-              Category: 'Automotive',
+var quotes = [{quote: ' When something is important enough, you do it even if the odds are not in your favor',
+              source: 'Elon Musk', 
+              citation: 'www.cbsnews.com ', 
+              year: 'May 2012', 
+              category: 'Automotive',
             }, 
               
-              {Quote: 'Design is a funny word. Some people think design means how it looks. But of course, if you dig deeper, it’s really how it works',  
-              Source: 'Steve Jobs', 
-              Citation: 'Wired Magazine', 
-              Year:  '1994', 
-              Category: 'Technology'
+              {quote: 'Design is a funny word. Some people think design means how it looks. But of course, if you dig deeper, it’s really how it works',  
+              source: 'Steve Jobs', 
+              citation: 'Wired Magazine', 
+              year:  '1994', 
+              category: 'Technology'
             }, 
 
-              { Quote: 'Rice is great when you’re hungry and you want 2000 of something.',  
-               Source: 'Mitch Hedberg', 
-               Citation: 'Unknown',
-               Year: ' Unknown ', 
-               Category: 'Comedy',
+              { quote: 'Rice is great when you’re hungry and you want 2000 of something.',  
+               source: 'Mitch Hedberg', 
+               citation: 'Unknown',
+               year: ' Unknown ', 
+               category: 'Comedy',
             },  
 
-            {Quote: 'Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.', 
-            Source:  'Michael Scott (Steve Carell),',
-            Citation: ' The Office, Season 2: The Fight.', 
-            Year: 'Unknown',  
-            Category: 'TV',
+            {quote: 'Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.', 
+            source:  'Michael Scott (Steve Carell),',
+            citation: ' The Office, Season 2: The Fight.', 
+            year: 'Unknown',  
+            category: 'TV',
               },
 
             {
-              Quote: 'If you obsess over whether you are making the right decision, you are basically assuming that the universe will reward you for one thing and punish you for another.', 
-              Source: 'Deepak Chopra',
-              Citation: 'The Book of Secrets: Unlocking the Hidden Dimensions of Your Life',
-              Year: '2005',
-              Category: 'Spiritual',
+              quote: 'If you obsess over whether you are making the right decision, you are basically assuming that the universe will reward you for one thing and punish you for another.', 
+              source: 'Deepak Chopra',
+              citation: 'The Book of Secrets: Unlocking the Hidden Dimensions of Your Life',
+              year: '2005',
+              category: 'Spiritual',
             }, 
 
 ];
  
-  var i = 0; i < quotes.length ; i ++; 
-    
-   {
-    var quote = quotes[i]+= quotes.Source + quotes.Citation + quotes.Year + quotes.Category;
-   }
-  
+
+
 
 
 /***
@@ -79,12 +75,11 @@ console.log(getRandomQuote());
 ***/ 
 function printQuote(){ 
   var randomQuote = getRandomQuote();
-  innerHTML = <p class = "quote"> + randomQuote.Quote</p>; 
-  return(randomQuote); 
+  var htmlString = '<p class = "quote"> '+ randomQuote.quote + randomQuote.source + randomQuote.citation + randomQuote.year + randomQuote.category +'</p>'; 
+  document.getElementById("quote-box").htmlstring = htmlString;  
+    
+
 } 
-
-document.getElementById("quote-box").innerHTML = printQuote; 
-
 console.log(printQuote()); 
  
    
